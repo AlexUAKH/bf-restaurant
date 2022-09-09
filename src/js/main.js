9c3785443532bd3fc4d8842088ff55fb806abc72
AlexUAKH <sashaos1404@gmail.com>
2022-09-09 16:38:43 +0300

    footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,22 @@
 import * as func from "./modules/func.js";
 
-console.log("Hoi");
 func.isWebp();
+const collapse = document.querySelectorAll(".collapse");
+
+let width = window.innerWidth;
+console.log(width);
+
+collapse.forEach((el) => {
+  if (width < 700) el.classList.remove("open");
+  else el.classList.add("open");
+  el.addEventListener("click", () => {
+    if (width < 700) {
+      collapse.forEach((el) => el.classList.remove("open"));
+      el.classList.toggle("open");
+    }
+  });
+});
+
+window.addEventListener("resize", () => {
+  width = window.innerWidth;
+});
