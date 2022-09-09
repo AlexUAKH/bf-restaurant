050bd72516c4793a61932e6fd7ecdda0ae70460c
AlexUAKH <sashaos1404@gmail.com>
2022-09-09 10:08:44 +0300

    header

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,10 +1,10 @@
-import dartSass from "sass";
-import gulpSass from "gulp-sass";
-import rename from "gulp-rename";
-import cleanCss from "gulp-clean-css";
-import webpcss from "gulp-webpcss";
 import autoprefixer from "gulp-autoprefixer";
+import cleanCss from "gulp-clean-css";
 import groupCssMediaQueries from "gulp-group-css-media-queries";
+import rename from "gulp-rename";
+import gulpSass from "gulp-sass";
+import webpcss from "gulp-webpcss";
+import dartSass from "sass";
 
 const sass = gulpSass(dartSass);
 
@@ -19,7 +19,7 @@ export const scss = () => {
         })
       )
     )
-    .pipe(app.plugins.replace(/@img\//g, "../img/"))
+    .pipe(app.plugins.replace(/@img\//g, "../imgwwww/"))
     .pipe(
       sass({
         outputStyle: "expanded"
@@ -45,6 +45,7 @@ export const scss = () => {
         })
       )
     )
+    .pipe(app.plugins.replace(/@img\//g, "../img/"))
     .pipe(app.gulp.dest(app.path.build.css))
     .pipe(app.plugins.if(app.isBuild, cleanCss()))
     .pipe(
