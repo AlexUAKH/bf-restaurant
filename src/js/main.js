247e0c8b846be5ec8c7f3c192aee2beb8644926b
AlexUAKH <sashaos1404@gmail.com>
2022-09-10 12:14:03 +0300

    fix bug and build

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,15 @@ func.isWebp();
 const collapse = document.querySelectorAll(".collapse");
 
 let width = window.innerWidth;
-console.log(width);
 
 collapse.forEach((el) => {
   if (width < 700) el.classList.remove("open");
   else el.classList.add("open");
-  el.addEventListener("click", () => {
+  el.addEventListener("click", (e) => {
     if (width < 700) {
-      collapse.forEach((el) => el.classList.remove("open"));
+      collapse.forEach((el) => {
+        if (el !== e.target) el.classList.remove("open");
+      });
       el.classList.toggle("open");
     }
   });
@@ -19,4 +20,14 @@ collapse.forEach((el) => {
 
 window.addEventListener("resize", () => {
   width = window.innerWidth;
+  if (width > 700) collapseHandler(collapse, true);
+  else collapseHandler(collapse, false);
 });
+
+function collapseHandler(elements, toOpen) {
+  console.log("tt: ", toOpen);
+  elements.forEach((el) => {
+    if (toOpen) el.classList.add("open");
+    else el.classList.remove("open");
+  });
+}
